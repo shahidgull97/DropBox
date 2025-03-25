05f6f5cea92c60eb8e0c81ce742daa1e12913c54
Shahid Gull <parrayshahid97@gmail.com>
2025-03-25 17:06:14 +0530

    added Typography animation

--- src/components/Color.tsx
+++ src/components/Color.tsx
@@ -1,20 +1,88 @@
 import React from "react";
+import { useRef } from "react";
+import gsap from "gsap";
 interface ColorProps {
   gridItemsRefs: React.MutableRefObject<(HTMLDivElement | null)[]>;
 }
 function Color({ gridItemsRefs }: ColorProps) {
+  const div1Ref = useRef<HTMLDivElement>(null);
+  const div2Ref = useRef<HTMLDivElement>(null);
+  const innediv1Ref = useRef<HTMLDivElement>(null);
+  const innerdiv2Ref = useRef<HTMLDivElement>(null);
+  const handleMouseEnter = () => {
+    gsap.to(div1Ref.current, {
+      border: "2px white solid",
+      backgroundColor: "black",
+      ease: "power2,out",
+      x: 60,
+      // transform: "translate(10,0)",
+    });
+    // move div2
+    gsap.to(div2Ref.current, {
+      border: "2px white solid",
+      backgroundColor: "black",
+      x: -72,
+      ease: "power2,out",
+    });
+    gsap.to(innediv1Ref.current, {
+      border: "2px white solid",
+      backgroundColor: "black",
+    });
+    gsap.to(innerdiv2Ref.current, {
+      border: "2px white solid",
+      backgroundColor: "black",
+    });
+  };
+  const handleMouseLeave = () => {
+    gsap.to(div1Ref.current, {
+      border: "none",
+      backgroundColor: "rgb(180, 83, 9)",
+      ease: "power2.out",
+      x: 0,
+    });
+    gsap.to(div2Ref.current, {
+      backgroundColor: "rgb(255, 160, 0)",
+      border: "none",
+      x: 0,
+      ease: "power2,out",
+    });
+    gsap.to(innediv1Ref.current, {
+      border: "none",
+      backgroundColor: "rgb(255, 160, 0)",
+    });
+    gsap.to(innerdiv2Ref.current, {
+      border: "none",
+      backgroundColor: "rgb(180, 83, 9)",
+    });
+  };
   return (
     <div
       ref={(el) => {
         gridItemsRefs.current[2] = el;
       }}
-      className="h-[54.5vh] w-[26vw] absolute left-[19.2vw] bottom-2 bg-amber-700 p-4 hover:bg-black transition-colors duration-500 text-white "
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      className="h-[54.5vh] w-[26vw] absolute left-[19.2vw] bottom-2 bg-amber-600 p-4 hover:bg-black transition-colors duration-500 text-white "
     >
       <div className="text-2xl font-bold text-white">Color</div>
-      <div className="flex items-center justify-center h-full">
-        <div className="border-2 border-white p-5">
-          <div className="w-16 h-16 rounded-full border-2 border-white"></div>
-          <div className="w-16 h-16 rounded-full border-2 border-white mt-2"></div>
+      <div className="flex flex-col items-end justify-end h-full mb-2">
+        <div
+          ref={div1Ref}
+          className="  p-2 w-17 h-17 bg-amber-700 absolute right-[6vw] bottom-[11vh] items-center justify-center"
+        >
+          <div
+            ref={innediv1Ref}
+            className=" relative w-[90%] bg-amber-900  h-[90%] rounded-2xl  "
+          ></div>
+        </div>
+        <div
+          ref={div2Ref}
+          className=" bg-amber-900 p-2 w-17 h-17 mb-6 items-center justify-center"
+        >
+          <div
+            ref={innerdiv2Ref}
+            className="w-[90%] h-[90%] rounded-2xl  bg-amber-700 m "
+          ></div>
         </div>
       </div>
     </div>
--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -1,18 +1,46 @@
 import React from "react";
+import { useRef } from "react";
+import gsap from "gsap";
 interface TypographyProps {
   gridItemsRefs: React.MutableRefObject<(HTMLDivElement | null)[]>;
 }
 function Typography({ gridItemsRefs }: TypographyProps) {
+  const textRef = useRef<HTMLSpanElement | null>(null);
+  const containerRef = useRef<HTMLDivElement | null>(null);
+
+  const handleMouseEnter = () => {
+    gsap.to(textRef.current, {
+      scale: 1.2, // Slightly increase the size
+      color: "transparent", // Hide the fill color
+      webkitTextStroke: "2px white", // Apply outline effect
+      duration: 0.5,
+      ease: "power2.out",
+    });
+  };
+
+  const handleMouseLeave = () => {
+    gsap.to(textRef.current, {
+      scale: 1,
+      color: "#424242", // Restore the original fill color
+      webkitTextStroke: "0px transparent",
+      duration: 0.3,
+      ease: "power2.out",
+    });
+  };
   return (
     <div
       ref={(el) => {
         gridItemsRefs.current[5] = el;
       }}
-      className="h-[40vh] w-[18.5vw] absolute right-2 top-2 bg-red-500 p-4 flex flex-col justify-between hover:bg-black transition-colors duration-500 text-white "
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      className="h-[40vh] w-[18.5vw] absolute right-2 top-2 bg-orange-600 p-4 flex flex-col justify-between hover:bg-black transition-colors duration-500 text-white "
     >
-      <div className="text-2xl font-bold">Typography</div>
-      <div className="flex items-center justify-center flex-1">
-        <span className="text-8xl font-bold text-gray-800">Aa</span>
+      <div className="text-4xl font-bold text-red-900">Typography</div>
+      <div className="flex items-end justify-end flex-1">
+        <span ref={textRef} className="text-8xl font-bold text-gray-800">
+          Aa
+        </span>
       </div>
     </div>
   );
